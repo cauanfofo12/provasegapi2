65881a6e560452b17bba6d891eff38e1049f33b0
cauanfofo12 <fortescauan@gmail.com>
2022-05-30 15:17:57 -0300

    oi

---
+++ api/src/index.js
@@ -0,0 +1,19 @@
+import 'dotenv/config'
+
+
+import express from 'express'
+import cors from 'cors'
+
+import { con } from './repository/connection.js';
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+
+
+
+
+
+
+server.listen(process.env.PORT, () => console.log (`API conectada na porta ${process.env.PORT}`))
---
+++ api/src/repository/usuarioRepository.js
@@ -0,0 +1,15 @@
+import { con } from './connection.js'
+
+
+export function login(email, senha ){
+    const comando = 
+    `select id_usuario 		id,
+    nm_usuario		nome,
+    ds_email			email
+from tb_usuario
+where ds_email 		= ?
+and ds_senha			= ?`
+
+
+    const resp = con.query(comando, [email, senha ])
+}
